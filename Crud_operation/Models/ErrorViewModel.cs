3a43c99959c882c3748771850e21aa9e83b1a4c8
Ramila Chanchiya <164127763+ramilachanchiya188@users.noreply.github.com>
2024-03-21 23:45:44 +0530

    Delete Crud_operation directory

--- Crud_operation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Crud_operation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Crud_operation/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Crud_operation.Models
-{
-    public class User
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
